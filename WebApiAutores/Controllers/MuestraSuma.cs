88da3ced0ecb5f396e7961a84d7914eb32dce44e
CARLITOS <rodrigo@idat.pe>
2022-01-30 21:10:16 -0500

    se agrego un controlador y una clase mas

---
+++ WebApiAutores/Controllers/MuestraSuma.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/auto")]
+    public class MuestraSuma: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Calculo>> Get()
+        {
+            return new List<Calculo>()
+            {
+                new Calculo() {num1 = 1, num2 = 2},
+                new Calculo() { num1 = 2, num2 = 3},
+            };
+        }
+    }
+}
---
+++ WebApiAutores/Entidades/Calculo.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Calculo
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+    }
+}
